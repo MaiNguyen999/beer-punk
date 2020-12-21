e23daa9ff687948e0af2732c0082c22e0aa5a242
MaiNguyen999 <maikhanhca2019@gmail.com>
2020-12-21 12:00:09 -0600

    adding beer lists

---
+++ app/src/main/java/com/example/punkbeer/BeerDetail.java
@@ -0,0 +1,14 @@
+package com.example.punkbeer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class BeerDetail extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_beer_detail);
+    }
+}
